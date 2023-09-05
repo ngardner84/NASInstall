bf4a52ee68f927c1da1707138e93cb7c0fa09899
Nathan Gardner <38595421+ngardner84@users.noreply.github.com>
2023-09-05 12:08:52 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ sudo cp $REPORT_FILE "$TARGET_DIR"
 rm $REPORT_FILE
 
 # Define the directory where the package resides
-PACKAGE_DIR="/Mac/Adobe/Installers/Creative Cloud for M1/"
+PACKAGE_DIR="/Mac/Adobe/Installers/Creative\ Cloud\ for\ M1/"
 
 # Find the first .pkg file in the directory
 PACKAGE_NAME=$(ls $PACKAGE_DIR/*.pkg | head -n 1)
